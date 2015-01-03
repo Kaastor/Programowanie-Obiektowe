11746b6962de51b8a6dd30ad3d18a4205e3f10d4
Kaastor <czuba.przemyslaw@gmail.com>
2015-01-03 20:07:56 +0100

    header

---
+++ Bajka/Miejsce.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "windows.h"
+#include "stdafx.h"
+#include "iostream"
+#include <string>
+using namespace std;
+
+class Miejsce
+{
+public:
+	string nazwa;
+	string opis;
+
+public:
+
+	Miejsce();
+	~Miejsce();
+};
