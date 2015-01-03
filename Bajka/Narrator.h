65540b1a2238860a4f490e894c68d189e0491da8
Kaastor <czuba.przemyslaw@gmail.com>
2015-01-03 20:08:26 +0100

    header

---
+++ Bajka/Narrator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "windows.h"
+#include "stdafx.h"
+#include "iostream"
+#include <string>
+using namespace std;
+
+class Narrator
+{
+
+public:
+	void opowiadanie(string Tab1[]);
+	~Narrator();
+	Narrator();
+};
